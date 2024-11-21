5495237785900363c9f5253e7275625323139146
polarvector <72918894+polarvector@users.noreply.github.com>
2024-11-21 10:45:09 +0000

    Marquee

--- app/page.tsx
+++ app/page.tsx
@@ -9,57 +9,12 @@ import FadeInImage from '@/components/ui/FadeInImage';
 // demo logos
 import {
 	Logo1,
-	Logo10,
-	Logo2,
-	Logo3,
-	Logo4,
-	Logo5,
-	Logo6,
-	Logo7,
-	Logo8,
-	Logo9,
 } from './logos';
 
 const logos = [
 	{
 		key: 'logo-1',
 		logo: Logo1,
-	},
-	{
-		key: 'logo-2',
-		logo: Logo2,
-	},
-	{
-		key: 'logo-3',
-		logo: Logo3,
-	},
-	{
-		key: 'logo-4',
-		logo: Logo4,
-	},
-	{
-		key: 'logo-5',
-		logo: Logo5,
-	},
-	{
-		key: 'logo-6',
-		logo: Logo6,
-	},
-	{
-		key: 'logo-7',
-		logo: Logo7,
-	},
-	{
-		key: 'logo-8',
-		logo: Logo8,
-	},
-	{
-		key: 'logo-9',
-		logo: Logo9,
-	},
-	{
-		key: 'logo-10',
-		logo: Logo10,
 	},
 ];
 
